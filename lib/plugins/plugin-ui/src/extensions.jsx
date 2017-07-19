d089004a8b9b0d9957a6b3783a51d8ccd3b85398
子昀 <yanhai.wyh@alibaba-inc.com>
2017-07-20 02:57:53 +0800

    up

--- lib/plugins/plugin-ui/src/extensions.jsx
+++ lib/plugins/plugin-ui/src/extensions.jsx
@@ -1,4 +1,4 @@
-import '/Users/aniwei/Desktop/Workspace/aniwei.proxy/lib/plugins/plugin-ssl/ui';
-import '/Users/aniwei/Desktop/Workspace/aniwei.proxy/lib/plugins/plugin-simulator/ui';
-import '/Users/aniwei/Desktop/Workspace/aniwei.proxy/lib/plugins/plugin-host/ui';
-import '/Users/aniwei/Desktop/Workspace/aniwei.proxy/lib/plugins/plugin-requester/ui';
+import '/Users/aniwei/Desktop/aniwei/aniwei.proxy/lib/plugins/plugin-ssl/ui';
+import '/Users/aniwei/Desktop/aniwei/aniwei.proxy/lib/plugins/plugin-simulator/ui';
+import '/Users/aniwei/Desktop/aniwei/aniwei.proxy/lib/plugins/plugin-host/ui';
+import '/Users/aniwei/Desktop/aniwei/aniwei.proxy/lib/plugins/plugin-requester/ui';
